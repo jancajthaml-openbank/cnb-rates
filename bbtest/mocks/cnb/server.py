91772de032fb2717354c85b056ba6fabb97b3978
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-08-09 09:59:49 +0200

    proper self signed certificates in blackbox tests

--- bbtest/mocks/cnb/server.py
+++ bbtest/mocks/cnb/server.py
@@ -3,10 +3,8 @@
 
 import threading
 from http.server import HTTPServer
+from helpers.encrypt import SelfSignedCeritifate
 import ssl
-import os
-import time
-import tempfile
 from .handler import RequestHandler
 from .logic import BussinessLogic
 
@@ -17,20 +15,13 @@ class CNBMock(threading.Thread):
     threading.Thread.__init__(self)
     self.context = context
     self.port = 4000
-    self.__keyfile = tempfile.NamedTemporaryFile()
-    self.__certfile = tempfile.NamedTemporaryFile()
 
-    # https://stackoverflow.com/questions/10175812/how-to-create-a-self-signed-certificate-with-openssl/27931596#27931596
-    # https://stackoverflow.com/questions/21297139/how-do-you-sign-a-certificate-signing-request-with-your-certification-authority/21340898#21340898
-
-    os.system("sed -i '/^.*v3_ca.*/a subjectAltName = IP:127.0.0.1' /etc/ssl/openssl.cnf")
-    os.system('faketime "1991-01-01 00:00:00" openssl req -x509 -nodes -newkey rsa:2048 -keyout "{}" -out "{}" -days 36500 -subj "/CN=127.0.0.1" > /dev/null 2>&1'.format(self.__keyfile.name, self.__certfile.name))
-    os.system('cp {} /usr/local/share/ca-certificates/ > /dev/null 2>&1'.format(self.__certfile.name))
-    os.system('update-ca-certificates > /dev/null 2>&1')
+    self.__certificate = SelfSignedCeritifate('cnb-mock')
+    self.__certificate.generate()
 
   def start(self):
     self.httpd = HTTPServer(('127.0.0.1', self.port), RequestHandler)
-    self.httpd.socket = ssl.wrap_socket(self.httpd.socket, certfile=self.__certfile.name, keyfile=self.__keyfile.name, server_side=True)
+    self.httpd.socket = ssl.wrap_socket(self.httpd.socket, certfile=self.__certificate.certfile, keyfile=self.__certificate.keyfile, server_side=True)
     self.httpd.logic = BussinessLogic()
     threading.Thread.start(self)
 
@@ -44,6 +35,5 @@ class CNBMock(threading.Thread):
       self.join()
     except:
       pass
-    self.__keyfile.close()
-    self.__certfile.close()
+    self.__certificate.cleanup()
     del self.httpd
