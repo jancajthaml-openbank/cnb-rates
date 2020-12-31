9c796b1449f57bfca124fe080b0cdc92ddeb2579
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-12-31 13:25:58 +0100

    improve debian package, and concurrency, improve coordinated shutdown and bbtests

--- bbtest/steps/orchestration_steps.py
+++ bbtest/steps/orchestration_steps.py
@@ -26,7 +26,6 @@ def timeshift(context, value):
   def wait_for_import_to_stop():
     (code, result, error) = execute(["systemctl", "stop", "cnb-rates-import.service"])
     assert code == 0, str(result) + ' ' + str(error)
-
     (code, result, error) = execute(["systemctl", "show", "-p", "SubState", 'cnb-rates-import.service'])
     assert code == 0, str(result) + ' ' + str(error)
     assert 'SubState=dead' in result, str(result) + ' ' + str(error)
@@ -45,40 +44,36 @@ def step_impl(context, package, operation):
   elif operation == 'uninstalled':
     (code, result, error) = execute(["apt-get", "-y", "remove", package])
     assert code == 0, "unable to uninstall with code {} and {} {}".format(code, result, error)
+    (code, result, error) = execute(["apt-get", "-y", "purge", package])
+    assert code == 0, "unable to purge with code {} and {} {}".format(code, result, error)
     assert os.path.isfile('/etc/cnb-rates/conf.d/init.conf') is False
   else:
-    assert False
+    assert False, 'unknown operation {}'.format(operation)
 
 
 @given('systemctl contains following active units')
 @then('systemctl contains following active units')
 def step_impl(context):
-  (code, result, error) = execute(["systemctl", "list-units", "--no-legend"])
-  assert code == 0
-
+  (code, result, error) = execute(["systemctl", "list-units", "--no-legend", "--state=active"])
+  assert code == 0, str(result) + ' ' + str(error)
   items = []
   for row in context.table:
     items.append(row['name'] + '.' + row['type'])
-
   result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
   result = [item for item in result if item in items]
-
   assert len(result) > 0, 'units not found'
 
 
 @given('systemctl does not contain following active units')
 @then('systemctl does not contain following active units')
 def step_impl(context):
-  (code, result, error) = execute(["systemctl", "list-units", "--no-legend"])
-  assert code == 0
-
+  (code, result, error) = execute(["systemctl", "list-units", "--no-legend", "--state=active"])
+  assert code == 0, str(result) + ' ' + str(error)
   items = []
   for row in context.table:
     items.append(row['name'] + '.' + row['type'])
-
   result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
   result = [item for item in result if item in items]
-
   assert len(result) == 0, 'units {} found'.format(result)
 
 
@@ -88,8 +83,8 @@ def unit_running(context, unit):
   @eventually(10)
   def wait_for_unit_state_change():
     (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
-    assert code == 0, code
-    assert 'SubState=running' in result, result
+    assert code == 0, str(result) + ' ' + str(error)
+    assert 'SubState=running' in result, str(result) + ' ' + str(error)
 
   wait_for_unit_state_change()
 
@@ -97,9 +92,13 @@ def unit_running(context, unit):
 @given('unit "{unit}" is not running')
 @then('unit "{unit}" is not running')
 def unit_not_running(context, unit):
-  (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
-  assert code == 0, str(result) + ' ' + str(error)
-  assert 'SubState=dead' in result, str(result) + ' ' + str(error)
+  @eventually(10)
+  def wait_for_unit_state_change():
+    (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
+    assert code == 0, str(result) + ' ' + str(error)
+    assert 'SubState=dead' in result, str(result) + ' ' + str(error)
+
+  wait_for_unit_state_change()
 
 
 @given('{operation} unit "{unit}"')
@@ -107,8 +106,6 @@ def unit_not_running(context, unit):
 def operation_unit(context, operation, unit):
   (code, result, error) = execute(["systemctl", operation, unit])
   assert code == 0, str(result) + ' ' + str(error)
-  if operation == 'restart':
-    unit_running(context, unit)
 
 
 @given('{unit} is configured with')
@@ -117,12 +114,3 @@ def unit_is_configured(context, unit):
   for row in context.table:
     params[row['property']] = row['value']
   context.unit.configure(params)
-
-  (code, result, error) = execute([
-    'systemctl', 'list-units', '--no-legend'
-  ])
-  result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
-  result = [item for item in result if ("{}-".format(unit) in item and ".service" in item)]
-
-  for unit in result:
-    operation_unit(context, 'restart', unit)
